94c799a06f4846d06bb3003aceef769d7cbda649
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2019-12-08 16:30:13 -0500

    v1.0-0

--- apace.R
+++ apace.R
@@ -1,17 +1,40 @@
-extract <- function(tarfile, files, exdir)
-        untar(tarfile=tarfile, files=files, exdir=exdir, compressed=F)
+tarxf <- function(tarfile, files, exdir)
+        untar(
+            tarfile=tarfile,
+            files=files,
+            exdir=exdir,
+            compressed=F)
 
-tartf <- function(tarfile) untar(tarfile=tarfile, list=T)
+tartf <- function(tarfile)
+        untar(
+            tarfile=tarfile,
+            list=T)
 
-install <- function(tarfile, files, exdir, lib) {
-    dir.create(exdir)
-    extract(tarfile, files, exdir)
+install <- function(tarfile, files, exdir.parent, exdir, lib) {
+    if (!dir.exists(exdir.parent)) {
+        warning("apace::install: exdir.parent does not exist: install aborted.")
+        return () }
+
+    EXDIR <- paste0(exdir.parent, exdir)
+
+    if (dir.exists(EXDIR)) {
+        warning("apace::install: exdir already exists: install aborted.")
+        return () }
+
+    dir.create(EXDIR)
+
+    tarxf(tarfile, files, EXDIR)
+
     install.packages(
-        pkgs=paste0(exdir, files),
+        pkgs=paste0(EXDIR, files),
         lib=lib,
         repos=NULL,
         method="internal",
         type="source",
         verbose=T)
-    unlink(exdir, recursive=T)
+
+    unlink(
+        # REMOVE TERMINAL PATH SEPARATOR
+        substr(EXDIR, 1, nchar(EXDIR) - 1),
+        recursive=T)
 }
