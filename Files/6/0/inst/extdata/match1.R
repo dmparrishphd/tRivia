272bf33472de4405db889b307d807f0b15e20518
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2020-10-23 15:00:53 -0400

    Create match1.R

---
+++ Files/6/0/inst/extdata/match1.R
@@ -0,0 +1,16 @@
+match1
+======
+
+Details
+-------
+
+Similar to `match`, but
+`x` and `table` must be single values,
+`nomatch` is effectively fixed at `FALSE`, and
+the return is a `logical` vector of length 1,
+indicating whether there was a match.
+
+Example
+-------
+
+    # (See documentation for `b.match1All`)
