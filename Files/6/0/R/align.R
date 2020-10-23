c30b874ee3d01a786b2bbf4e3c8f1ed8d02157eb
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2020-10-23 14:49:39 -0400

    Create align.R

---
+++ Files/6/0/R/align.R
@@ -0,0 +1,11 @@
+align <- function(..., Match=match, Unique=unique) {
+	  X <- list(...)
+	  VALUES <- Unique(do.call(c, X))
+	  FUN.VALUE <- matrix(as.list(VALUES))
+	  FUN <- function(table)
+		    matrix(lapply(VALUES, function(x) Match(x, table)))
+	  INDICES <- lapply(X, FUN)
+	  names(INDICES) <- paste0("I", seq_along(X))
+	  data.frame(INDICES, VALUE=matrix(VALUES), check.names=F,
+		    stringsAsFactors=F) }
+
