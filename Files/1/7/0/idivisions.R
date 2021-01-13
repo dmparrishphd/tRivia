067d71e4e467a86eb92b9479f3c2cf093d358d00
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-01-13 15:36:41 -0500

    Update idivisions.R

--- Files/1/7/0/idivisions.R
+++ Files/1/7/0/idivisions.R
@@ -1,3 +1,3 @@
 idivisions <- function(from=1, to, by=2L) {
-	s <- seq.int(from, to, by)
-	cbind(s, c(s[-1] - 1L, to), deparse.level=0) }
+    s <- seq.int(from, to, by)
+    cbind(s, c(s[-1] - 1L, to), deparse.level=0) }
