58f04ca7a6eb17f22d384150313a6702bf15743a
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-01-16 19:40:42 -0500

    Update scanLinesVerbatim.R

--- Files/3/1/0/scanLinesVerbatim.R
+++ Files/3/1/0/scanLinesVerbatim.R
@@ -9,6 +9,7 @@ scanLinesVerbatim <- function(file="", n=-1, skip=0, quiet=TRUE,
         skipNul=skipNul)
     Option <- if (missing(file) && is.character(text) && length(text) == 1)
         list(text=text) else list()
+    if (length(Option)) AdditionalArgs$file <- NULL
     Args <- concatenate.lists(
         .scanLinesVerbatim.Args.fixed, AdditionalArgs, Option)
     do.call(scan, Args) }
