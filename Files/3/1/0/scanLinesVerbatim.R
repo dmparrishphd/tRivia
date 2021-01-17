edb99c2f98a4801e01b0eabc1492a48b717a4e6a
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-01-16 19:20:24 -0500

    Create scanLinesVerbatim.R

---
+++ Files/3/1/0/scanLinesVerbatim.R
@@ -0,0 +1,15 @@
+.scanLinesVerbatim.Args.fixed <- list(what="", sep="\n",
+        na.strings=character(), blank.lines.skip=F)
+
+
+scanLinesVerbatim <- function(file="", n=-1, skip=0, quiet=TRUE,
+        fileEncoding="", encoding="unknown", text=character(),
+        skipNul=TRUE) {
+    AdditionalArgs <- list(file=file, n=n, skip=skip,
+        fileEncoding=fileEncoding, encoding=encoding,
+        skipNul=skipNul)
+    Option <- if (missing(file) && is.character(text) && length(text) == 1)
+        list(text=text) else list()
+    Args <- concatenate.lists(
+        .scanLinesVerbatim.Args.fixed, AdditionalArgs, Option)
+    do.call(scan, Args) }
