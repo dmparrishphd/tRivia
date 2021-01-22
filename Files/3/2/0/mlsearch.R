658bb46a69deb28a55768d9887894d6785eac2c3
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-01-22 15:26:00 -0500

    Update mlsearch.R

--- Files/3/2/0/mlsearch.R
+++ Files/3/2/0/mlsearch.R
@@ -1,6 +1,5 @@
 mlsearch <- function ( x , table , compar = `==` ,
-    extract = `[[` , n = length ( table ) , USE.NAMES = F )
-            vapply ( FUN.VALUE = T , USE.NAMES = USE.NAMES ,
+    extract = `[[` , n = length ( table ) )
+            vapply ( FUN.VALUE = T , USE.NAMES = F ,
                 X = seq_len ( n ) , FUN = function ( i )
                     compar ( extract ( table , i ) , x ) )
-                    
