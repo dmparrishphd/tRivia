8388d8e51037dd6d9814d02ca5feb522e74c282a
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-02-17 09:08:24 -0500

    Update classificationChart.R

--- Dev/classificationChart.R
+++ Dev/classificationChart.R
@@ -1,2 +1,8 @@
 classificationChart <- function ( X , FUNS )
 		lapply ( FUNS , function ( fun ) which ( fun ( X ) ) )
+
+vectorize.classifications <- function ( X ) {
+	Classes <- integer ( max ( unlist ( X ) ) )
+	for ( i in seq_along ( X ) ) Classes [ X [[ i ]] ] <- i
+	Classes }
+
