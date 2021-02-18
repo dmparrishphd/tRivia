93519a4c61c28bc337770e64ee82a26beaf5f8de
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-02-18 08:20:57 -0500

    Update classificationChart.R

--- Dev/classificationChart.R
+++ Dev/classificationChart.R
@@ -1,9 +1,13 @@
-classificationChart <- function ( X , FUNS )
-		lapply ( FUNS , function ( fun ) which ( fun ( X ) ) )
-
-# ASSUMPTION: VALUES IN unlist (X) ARE UNIQUE			
-vectorize.classifications <- function ( X ) {
-	Classes <- integer ( max ( unlist ( X ) ) )
-	for ( i in seq_along ( X ) ) Classes [ X [[ i ]] ] <- i
-	Classes }
-
+CLASSIFICATION.CHART.PROTO <- list (
+	init = function ( . , X , FUNS ) {
+		.$.DATA <- lapply ( FUNS , function ( fun ) which ( fun ( X ) ) )
+	    . } ,
+	as.vector = function ( . )
+		unlist ( lapply (
+			seq_along ( .$.DATA ) ,
+			function ( i ) rep.int ( i , length ( .$.DATA [[ i ]] ) ) ) ) ,
+	as.data.frame = function ( . )
+        data.frame (
+			i = unlist ( .$.DATA ) ,
+			class = vectorize.classifications ( .$.DATA ) ) ,
+	.NULL = NULL )
