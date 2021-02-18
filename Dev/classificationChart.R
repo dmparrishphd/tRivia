d1268e79772f4fd3f9a23fc02907a5d700fd10b5
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-02-18 09:56:57 -0500

    Update classificationChart.R

--- Dev/classificationChart.R
+++ Dev/classificationChart.R
@@ -1,13 +1,12 @@
 CLASSIFICATION.CHART.PROTO <- list (
-	init = function ( . , X , FUNS ) {
-		.$.DATA <- lapply ( FUNS , function ( fun ) which ( fun ( X ) ) )
-	    . } ,
-	as.vector = function ( . )
-		unlist ( lapply (
-			seq_along ( .$.DATA ) ,
-			function ( i ) rep.int ( i , length ( .$.DATA [[ i ]] ) ) ) ) ,
-	as.data.frame = function ( . )
+    init = function ( . , X , FUNS ) {
+        .$.DATA <- lapply ( FUNS , function ( fun ) which ( fun ( X ) ) )
+        . } ,
+    as.vector = function ( . ) unlist ( lapply (
+        seq_along ( .$.DATA ) ,
+        function ( i ) rep.int ( i , length ( .$.DATA [[ i ]] ) ) ) ) ,
+    as.data.frame = function ( . )
         data.frame (
-			i = unlist ( .$.DATA ) ,
-			class = vectorize.classifications ( .$.DATA ) ) ,
-	.NULL = NULL )
+            i = unlist ( .$.DATA ) ,
+            class = vectorize.classifications ( .$.DATA ) ) ,
+    .NULL = NULL )
