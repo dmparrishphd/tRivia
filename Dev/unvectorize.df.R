42bf8caa8ebfb066f19971174e8315bc638ec18c
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-03-17 13:42:12 -0400

    Update unvectorize.df.R

--- Dev/unvectorize.df.R
+++ Dev/unvectorize.df.R
@@ -1,6 +1,7 @@
-unvectorize <- function ( .df , .... ) {
+unvectorize <- function ( .df , ... , USE.ROW.NAMES = FALSE ) {
 	DF <- data.frame ( NA ) [ , -1 ] # ONE ROW, NO COLUMNS
-    n <- nrow ( .df )
+	n <- nrow ( .df )
 	for ( i in seq_len ( n ) ) DF <- data.frame ( Y , .df [ i , ] , ... )
-	names ( DF ) <- paste ( names ( .df ) , rep ( seq_len ( ncol ( .df ) ) , each = n ) , sep = "" )
+	SUFFIX <- if ( USE.ROW.NAMES ) rownames ( .df ) else seq_len ( n )
+	names ( DF ) <- paste ( names ( .df ) , rep ( SUFFIX , each = n ) , sep = "" )
 	DF }
