abdb6d24a786b5e09a5488437f2a4169b47bb722
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-03-18 08:43:53 -0400

    Update rankrev.R

--- Dev/rankrev.R
+++ Dev/rankrev.R
@@ -1,20 +1,21 @@
 
 rankrev <- ( function () {
 
-    Env <- new.env ( parent = baseenv() )
+    Env <- new.env ( parent = baseenv () )
+    
     attr ( Env , "name" ) <- "rankrevEnv"
 
     Env $ .METHODS <- data.frame ( stringsAsFactors = FALSE ,
-        key = c ( "average" , "first" ,  "last" , "random" , "max" , "min" ) ,
+        key   = c ( "average" , "first" ,  "last" , "random" , "max" , "min" ) ,
         value = c ( "average" ,  "last" , "first" , "random" , "min" , "max" ) )
 
-    rankrev <- function ( x , na.last = TRUE , ties.method = c (
+    Fun <- function ( x , na.last = TRUE , ties.method = c (
         "average" , "first" ,  "last" , "random" , "max" , "min" ) ) {
-        LOOK <- parent.env(environment()) $ .METHODS
+        LOOK <- parent.env ( environment () ) $ .METHODS
         ties <- LOOK $ value [[ match ( ties.method , LOOK $ key ) ]]
         i <- rank ( x , na.last = na.last , ties.method = ties )
         1L + max ( i ) - i }
 
-    environment ( rankrev ) <- Env
+    environment ( Fun ) <- Env
 
-    rankrev } ) ()
+    Fun } ) ()
