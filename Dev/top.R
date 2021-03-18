43a6b163c31d5bf7e3efbc9c8d2bfb8e3e1eec5d
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-03-18 12:33:06 -0400

    Delete top.R

--- Dev/top.R
+++
@@ -1,9 +0,0 @@
-#TOP10, TOP3, ETC.
-#STATUS: VERY UNFINISHED
-top <- function ( X , n = 1 , j = 1 ) {
-	Chosen <- TRUE
-	ORDER <- do.call ( order ,  ( - ( X $ AREA ) )
-	if ( nrow ( X ) <= n ) return ( X )
-	X <- X [ ORDER , ]
-	X [ -3 : -1 , "CHOSEN" ] <- FALSE
-	X }
