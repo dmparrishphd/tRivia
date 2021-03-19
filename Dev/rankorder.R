35dc14be1dac4b27ba8120c5e0e1fc45a2144acd
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-03-19 12:21:14 -0400

    Update rankorder.R

--- Dev/rankorder.R
+++ Dev/rankorder.R
@@ -6,13 +6,16 @@ rankorder <- function ( ... , na.last = TRUE , decreasing = FALSE ,
     ARGC <- length ( X )
     na.last <- rep_len ( na.last , length.out = ARGC )
     decreasing <- rep_len ( decreasing , length.out = ARGC )
-    rank. <- c ( rank , rankrev ) [ 1L + decreasing ]
-    FUN <- function ( i ) rank. [[ i ]] (
+    RANK <- c ( rank , rankrev ) [ 1L + decreasing ]
+    FUN <- function ( i ) RANK [[ i ]] (
         x = X [[ i ]] ,
         ties.method = "min" ,
         na.last = na.last [[ i ]] )
     FUN.VALUE <- X [[ 1 ]] %|% length %|% double
-    Y <- vapply ( FUN = FUN , USE.NAMES = FALSE , X = X %|% seq_along ,
+    RANKS <- vapply (
+        FUN = FUN ,
+        USE.NAMES = FALSE ,
+        X = X %|% seq_along ,
         FUN.VALUE = FUN.VALUE )
-    ARGS <- c ( method = method , Y %|% as.data.frame )
+    ARGS <- c ( method = method , RANKS %|% as.data.frame )
     do.call ( order , ARGS ) }
