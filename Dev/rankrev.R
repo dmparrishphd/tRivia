8681ada7e40c6d9baba21170f5db2388ec4cf309
dmparrishphd <39537744+dmparrishphd@users.noreply.github.com>
2021-03-19 08:35:42 -0400

    Update rankrev.R

--- Dev/rankrev.R
+++ Dev/rankrev.R
@@ -1,19 +1,22 @@
-
 rankrev <- ( function () {
 
-    Env <- new.env ( parent = baseenv () )
-    
+    Env <- new.env ( parent = baseenv() )
     attr ( Env , "name" ) <- "rankrevEnv"
 
-    Env $ .METHODS <- data.frame ( stringsAsFactors = FALSE ,
-        key   = c ( "average" , "first" ,  "last" , "random" , "max" , "min" ) ,
-        value = c ( "average" ,  "last" , "first" , "random" , "min" , "max" ) )
+     Env $ .TIES.METHOD <- c ( 
+         average = "average" ,
+         first   = "last"    ,
+         last    = "first"   ,
+         random  = "random"  ,
+         max     = "min"     ,
+         min     = "max"     )
 
     Fun <- function ( x , na.last = TRUE , ties.method = c (
-        "average" , "first" ,  "last" , "random" , "max" , "min" ) ) {
-        LOOK <- parent.env ( environment () ) $ .METHODS
-        ties <- LOOK $ value [[ match ( ties.method , LOOK $ key ) ]]
-        i <- rank ( x , na.last = na.last , ties.method = ties )
+            "average" , "first" ,  "last" , "random" , "max" , "min" ) ) {
+        i <- rank (
+            x = x ,
+            na.last = na.last ,
+            ties.method = .TIES.METHOD [[ match.arg ( ties.method ) ]] )
         1L + max ( i ) - i }
 
     environment ( Fun ) <- Env
